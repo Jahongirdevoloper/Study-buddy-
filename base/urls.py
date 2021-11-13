2d8df8453326cc1570e3fab3c1a7b2a07fbb2ae1
Jahon <jahongirjorayev9505@gmail.com>
2021-11-13 12:10:37 +0500

    first commit

---
+++ base/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from base import views
+
+urlpatterns = [
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+
+    path('', views.home, name='home'),
+    path('room/<int:pk>/', views.room, name='room'),
+    path('profile/<int:pk>/', views.user_profile, name='user-profile'),
+
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<int:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<int:pk>/', views.delete_room, name='delete-room'),
+    path('delete-message/<int:pk>/', views.delete_message, name='delete-message'),
+
+    path('update-user/', views.update_user, name='update-user'),
+
+    path('topics/', views.topics_page, name='topics'),
+    path('activity/', views.activity_page, name = 'activity'),
+]
+
